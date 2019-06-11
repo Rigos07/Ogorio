a7fe2e7b04997ef7c1b45f95dc1f89370badc860
Rigos07 <51322232+Rigos07@users.noreply.github.com>
2019-06-11 10:49:43 +0200

    Update structures.h

--- M. PRθP⁻¹/structures.h
+++ M. PRθP⁻¹/structures.h
@@ -1,12 +1,14 @@
+#define BLUE_SIGHT 1000
+#define MAXLEN 26
+
 typedef struct Point {
     int x, y;
 } Point;
 
 typedef struct Path {
-    int nb_points;
-    Path *prev;
+    struct Path *prev;
     Point point;
-    Path *next;
+    struct Path *next;
 } Path;
 
 typedef struct Node {
@@ -16,7 +18,21 @@ typedef struct Node {
 } Node;
 
 typedef struct NodeList {
-    int nb_nodes;
     Node node;
-    nodelist *next;
+    struct NodeList *next;
 } NodeList;
+
+
+Path *create_path();
+Path *add_point(Path *last_element, Point new_point);
+Path *get_point(Path *head,int index);
+int get_path_size(Path *head);
+
+Node *create_node();
+
+NodeList *create_nodelist();
+NodeList *add_node(NodeList *last_element, Node new_node);
+int delete_node(NodeList *list, unsigned char id);
+int update_node(NodeList *list, unsigned char id , Node node);
+int get_nodelist_size(Path *head);
+Node *get_node(NodeList *list, unsigned char id);

fde6053821f733a3da4d561777e75bc09ef19609
Rigos07 <51322232+Rigos07@users.noreply.github.com>
2019-06-14 16:14:14 +0200

    greeeen

--- purple green experimental/green.h
+++ purple green experimental/green.h
@@ -1,8 +1,10 @@
 
 #define GREEN_SIGHTX 400
 #define GREEN_SIGHTY 400
+#define SHEEPFOLD_RADIUS 900
 #include "../communs_final/struct.h"
 
 Node green_node; 
 Dog green_dog;
 Path * path;
+Point sheepfold_center;

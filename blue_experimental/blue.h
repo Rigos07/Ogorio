50b79ddbdd9793d23d1630784ba0bc03089dc0ce
Rigos07 <51322232+Rigos07@users.noreply.github.com>
2019-06-16 17:48:23 +0200

    blue
    ajout de test pour blue

---
+++ blue_experimental/blue.h
@@ -0,0 +1,12 @@
+
+#define BLUE_SIGHTX 1000
+#define BLUE_SIGHTY 1000
+#include "../communs_final/struct.h"
+
+Node blue_node;
+Dog blue_dog;
+Path * path;
+
+
+Point sheepfold_center;
+int sheepfold_radius;

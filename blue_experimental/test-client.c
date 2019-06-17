c6654f95a90367a30dbc5afebdba5c0a657e3141
Rigos07 <51322232+Rigos07@users.noreply.github.com>
2019-06-17 22:18:58 +0200

    hehe

--- blue_experimental/test-client.c
+++ blue_experimental/test-client.c
@@ -35,7 +35,7 @@ Point Blue_behavior(Dog *blue, NodeList **nodes_in_sight){
 				if(!blue->message.done){
 					//printf("J'ENVOIE : id : %d pos : %d , %d\n", blue->message.id,blue->message.position.x,blue->message.position.y);
 					printf("OK ALORS : size i : %d id i : %d x i : %d y i : %d\n", blue->message.size_i,blue->message.id_i,blue->message.x_i,blue->message.y_i);
-					objective = encode_msg(&(blue->message));
+					objective = encode_msg(blue);
 					printf("OBJECTIF : %d , %d\n", objective.x, objective.y);
 				}
 				else{

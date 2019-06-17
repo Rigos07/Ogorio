e87ec60da5eb54e4db9b99b3120cdb383ce27354
Rigos07 <51322232+Rigos07@users.noreply.github.com>
2019-06-17 10:08:56 +0200

    fdes opezgjiezg

--- yellow_experimental/test-client.c
+++ yellow_experimental/test-client.c
@@ -54,11 +54,11 @@ Point Yellow_behavior(Dog *yellow, NodeList **nodes_in_sight){
 	NodeList *pointer = *nodes_in_sight;
 	Point blue_pos = create_point(0,0);
 	float distance_to_destination;
-	printf("GEHUIHUIEGIHZIHUEZ\n");
 	if(is_near_point(yellow->node.position, create_point(4500,3000), MARGIN)){
 		if(nodes_in_sight != NULL){
 			printlist(&nodes_in_sight);
 			while(pointer != NULL){
+				printnode(pointer->node);
 				if(!strcmp(pointer->node.nickname, "blue")){
 					blue_pos = pointer->node.position;
 				}

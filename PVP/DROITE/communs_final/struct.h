acc50371cefc2cc3aeb19a9239fe9d0a4d971792
userir <userir@jessie.esisar.fr>
2019-06-19 16:49:18 +0200

    VALENTIN T MOVAIS

--- PVP/DROITE/communs_final/struct.h
+++ PVP/DROITE/communs_final/struct.h
@@ -209,7 +209,7 @@ Point follow_path(Path **head, Dog dog, float max_dist);
 
 int is_pushed_by_yellow(NodeList** head, Node n);
 
-int is_pushed_by_green(NodeList** head, Node n)
+int is_pushed_by_green(NodeList** head, Node n);
 
 void sheep_count(Dog* dog, NodeList** head, Point ally_sheepfold_center, Point ennemy_sheepfold_center,int sheepfold_radius);
 
